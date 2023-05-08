b8d83ef508f11bfb2e945411d186ab01341b553f
Ilnas <genatula@protonmail.com>
2023-05-08 20:00:12 +0300

    Initial commit

---
+++ src/test/java/company/vk/genatulin/internshiptask/InternshipTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package company.vk.genatulin.internshiptask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
